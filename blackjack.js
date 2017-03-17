2fd1e14a82d76a2c9559f19cc71c7139615f11cd
Andrew Scheuermann <andrewscheuermann@gmail.com>
2017-03-17 14:12:14 -0700

    organize this.____ variables alphabetically

--- blackjack.js
+++ blackjack.js
@@ -4,7 +4,6 @@ TODO
   need ability to convert aces from 11 to 1 conditionally (force it if over 21), do it progressively
   console.log better welcome screen, instructions, and reset messaging
   rename variables to with _someName if "private" but still needed to expose for testing (maybe find other ways)
-  organize this.____ variables alphabetically
   add internal controls so user can't deal twice, hit before deal, hit after stay, hit beyond limit, etc
   add cumulative numbers to the console. ex: 'You are showing three and four and nine (16)'
 */
@@ -18,70 +17,34 @@ const log = (str) => {
 var Blackjack = function(passedPlayerHand, passedDealerHand) {
 
   /*
-    rules (https://www.youtube.com/watch?v=5bWpnABkU-Y)
+    VALUES 
     cards 2-10 are worth their face value
     Jack, Queen, King are worth 10
     Ace can be 1 or 11
 
-    gameplay
+    GAMEPLAY
     a dealer is dealt one card face up and one card face down
     a player is dealt two cards face up and can choose to from one of 5 options
     hit()
       take another card from the dealer
     stand() (aka stay)
       take no more cards. compare values
+
+    source -> https://www.youtube.com/watch?v=5bWpnABkU-Y
   */
 
-  // the ____Hand variables are internal only and will contain an object
+  // PRIVATE VARIABLES
+
   // {number, suit, blackjackValue, displayValue}
   let playerHand = passedPlayerHand || [];
   let dealerHand = passedDealerHand || [];
 
-  this.getBlackjackValue = function(n) {
-    if (n <= 10) {
-      return n;
-    } else if (n <= 13) {
-      return 10;
-    } else {
-      return 11;
-    }
-  }
-  this.getDisplayValue = function(n) {
-    const displayValueArray = [null, null, 'two', 'three', 'four', 'five', 'six',
-        'seven', 'eight', 'nine', 'ten', 'jack', 'queen', 'king', 'ace'];
-    return displayValueArray[n];
-  }
-
-  this.getCard = function() {
-    function randomIntFromInterval(min,max) {
-      // http://stackoverflow.com/a/7228322/3304337
-      return Math.floor(Math.random()*(max-min+1)+min);
-    }
-
-    const upperLimit = 14; // Ace
-    const lowerLimit = 2; // 2
-    const suits = ['hearts', 'spades', 'clubs', 'diamonds'];
-    return {
-      number: randomIntFromInterval(lowerLimit, upperLimit),
-      suit: suits[randomIntFromInterval(0, 3)],
-    }
-  };
-  this.createCardObject = function(passedCardObject) {
-    const cardObject = passedCardObject || this.getCard();
-    cardObject.blackjackValue = this.getBlackjackValue(cardObject.number);
-    cardObject.displayValue = this.getDisplayValue(cardObject.number);
-    return cardObject;
-  }
-
-  this.logTable = function() {
-    const dealerHandString = dealerHand.map(c => c.displayValue).join(' and ');
-    const playerHandString = playerHand.map(c => c.displayValue).join(' and ');
-    log(`The Dealer is showing ${dealerHandString}.\n    You are showing ${playerHandString}.`);
-  }
   function resetTable() {
     playerHand = [];
     dealerHand = [];
   }
+
+  // PUBLIC VARIABLES
   this.checkTable = function(playerHasStayed) {
     const playerPoints = playerHand.reduce((n, c) => n + c.blackjackValue, 0);
     const dealerPoints = dealerHand.reduce((n, c) => n + c.blackjackValue, 0);
@@ -103,13 +66,16 @@ var Blackjack = function(passedPlayerHand, passedDealerHand) {
       resetTable()
     } 
 
-    return {
-      playerPoints,
-      dealerPoints,
-      isWinner,
-    }
-    
+    return { playerPoints, dealerPoints, isWinner };
   }
+
+  this.createCardObject = function(passedCardObject) {
+    const cardObject = passedCardObject || this.getCard();
+    cardObject.blackjackValue = this.getBlackjackValue(cardObject.number);
+    cardObject.displayValue = this.getDisplayValue(cardObject.number);
+    return cardObject;
+  }
+
   this.deal = function() {
     log('-------------------');
     
@@ -124,6 +90,37 @@ var Blackjack = function(passedPlayerHand, passedDealerHand) {
     this.logTable();
     this.checkTable();
   }
+  
+  this.getBlackjackValue = function(n) {
+    if (n <= 10) {
+      return n;
+    } else if (n <= 13) {
+      return 10;
+    } else {
+      return 11;
+    }
+  }
+  
+  this.getCard = function() {
+    function randomIntFromInterval(min,max) {
+      // http://stackoverflow.com/a/7228322/3304337
+      return Math.floor(Math.random()*(max-min+1)+min);
+    }
+
+    const upperLimit = 14; // Ace
+    const lowerLimit = 2; // 2
+    const suits = ['hearts', 'spades', 'clubs', 'diamonds'];
+    return {
+      number: randomIntFromInterval(lowerLimit, upperLimit),
+      suit: suits[randomIntFromInterval(0, 3)],
+    }
+  };
+  
+  this.getDisplayValue = function(n) {
+    const displayValueArray = [null, null, 'two', 'three', 'four', 'five', 'six',
+        'seven', 'eight', 'nine', 'ten', 'jack', 'queen', 'king', 'ace'];
+    return displayValueArray[n];
+  }
 
   this.hit = function() {
     playerHand.push(this.createCardObject());
@@ -133,6 +130,12 @@ var Blackjack = function(passedPlayerHand, passedDealerHand) {
     this.logTable();
     this.checkTable();
   }
+
+  this.logTable = function() {
+    const dealerHandString = dealerHand.map(c => c.displayValue).join(' and ');
+    const playerHandString = playerHand.map(c => c.displayValue).join(' and ');
+    log(`The Dealer is showing ${dealerHandString}.\n    You are showing ${playerHandString}.`);
+  }  
 
   this.stay = function() {
     dealerHand.push(this.createCardObject());
